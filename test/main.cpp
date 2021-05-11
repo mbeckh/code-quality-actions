2e696a82d272b492cdd281eeb79d59a0d9119737
Michael Beckh <8502359+mbeckh@users.noreply.github.com>
2021-05-11 03:08:18 +0200

    Spaces instead of tabs

--- test/main.cpp
+++ test/main.cpp
@@ -6,21 +6,21 @@
 
 int called_always() {
 #ifdef _DEBUG
-	const char sz[] = "Debug";   // testing: deliberate error for clang-tidy
+    const char sz[] = "Debug";   // testing: deliberate error for clang-tidy
 #else
-	const char sz[] = "Release"; // testing: deliberate error for clang-tidy
+    const char sz[] = "Release"; // testing: deliberate error for clang-tidy
 #endif
-	return sz[0];
+    return sz[0];
 }
 
 int called_optional() {  // testing: no coverage
-	return 1;            // testing: no coverage
+    return 1;            // testing: no coverage
 }                        // testing: no coverage
 
 int main(int argc, char**) {
-	int result = called_always() == 'Z' ? 1 : 0;
-	if (argc > 1) {
-		result += called_optional();  // testing: no coverage
-	}
-	return result;
+    int result = called_always() == 'Z' ? 1 : 0;
+    if (argc > 1) {
+        result += called_optional();  // testing: no coverage
+    }
+    return result;
 }
