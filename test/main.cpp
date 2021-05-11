3c2bab6b7f5a83d3c6fc3f558e45708db0db7d96
Michael Beckh <8502359+mbeckh@users.noreply.github.com>
2021-05-11 03:02:13 +0200

    Add comments for testing

--- test/main.cpp
+++ test/main.cpp
@@ -1,20 +1,20 @@
 int called_always() {
 #ifdef _DEBUG
-	const char sz[] = "Debug";   // deliberate error for testing clang-tidy
+	const char sz[] = "Debug";   // testing: deliberate error for clang-tidy
 #else
-	const char sz[] = "Release"; // deliberate error for testing clang-tidy
+	const char sz[] = "Release"; // testing: deliberate error for clang-tidy
 #endif
 	return sz[0];
 }
 
-int called_optional() {
-	return 1;
-}
+int called_optional() {  // testing: no coverage
+	return 1;            // testing: no coverage
+}                        // testing: no coverage
 
 int main(int argc, char**) {
 	int result = called_always() == 'Z' ? 1 : 0;
 	if (argc > 1) {
-		result += called_optional();
+		result += called_optional();  // testing: no coverage
 	}
 	return result;
 }
